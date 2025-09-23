01d853062d4a61ad20ea62ed597a2d158412d742
Paulo-Vitor-dev <paulov9517@gmail.com>
2025-09-23 20:30:27 -0300

    mobile responsivo

--- script.js
+++ script.js
@@ -69,11 +69,11 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = '/img/menu_white_36dp.svg';
+        document.querySelector('.icon').src = 'img/image-Photoroom (5).png';
     }
     else { 
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = '/img/close_white_36dp.svg';
+        document.querySelector('.icon').src = 'img/image-Photoroom (7).png';
 
     }
 }
